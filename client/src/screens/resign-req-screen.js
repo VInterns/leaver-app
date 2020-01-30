ae95b8d127f246f8e1480d10008610f161200522
islam-gad2 <islam.gad2@vodafone.com>
2020-01-30 10:55:34 +0200

    Review and handling some errors

--- client/src/screens/resign-req-screen.js
+++ client/src/screens/resign-req-screen.js
@@ -15,135 +15,142 @@ export class ResignReqScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        staffId: '',
-        returnedHeadset: false,
-        returnedKeys: false,
-        returnedOhda: false,
-        ohdaType:'',
-        lastWorkDay:'',
-        nationalId:'',
-        nationalIdImg:null,
-        annualsGranted:'',
-        annualsTaken:'',
-        noShow:'',
-        lostHours:'',
-        daysToTake:'',
+      staffId: '',
+      returnedHeadset: false,
+      returnedKeys: false,
+      returnedOhda: false,
+      ohdaType: '',
+      lastWorkDay: '',
+      nationalId: '',
+      nationalIdImg: null,
+      annualsGranted: '',
+      annualsTaken: '',
+      noShow: '',
+      lostHours: '',
+      daysToTake: '',
 
-        sapID:'',
-        employeeName:'',
-        managerName:'',
-        ntAccount:'',
-        department:'',
-        careCenter:'',
-        jobTitle:'',
-        hiringDate:'',
-        mobNumber:'',
-        iex:'',
+      sapID: '',
+      employeeName: '',
+      managerName: '',
+      ntAccount: '',
+      department: '',
+      careCenter: '',
+      jobTitle: '',
+      hiringDate: '',
+      mobNumber: '',
+      iex: '',
     };
   }
-  
+
   onSearch = (e) => {
     e.preventDefault();
-      fetch(API + SEARCH, {
-        body: JSON.stringify({staffId:this.state.staffId}),
-        headers: {
-          'content-type': 'application/json'
-        },
-        method: 'POST',
-        // mode: 'cors',
+    fetch(API + SEARCH, {
+      body: JSON.stringify({ staffId: this.state.staffId }),
+      headers: {
+        'content-type': 'application/json'
+      },
+      method: 'POST',
+      // mode: 'cors',
+    })
+      .then((response) => {
+        if (response.status === 200) {
+
+          return (response.json());
+        } else {
+          alert('User Not found');
+          return undefined;
+        }
       })
-        .then( (response) => {
-          console.log('toot-client');
-          return(response.json());
-        })
-        .then( (data) => {
-            console.log(data.staffId);
-          this.setState({sapID:data.sapID});
-          this.setState({employeeName:data.employeeName});
-          this.setState({managerName:data.managerName});
-          this.setState({ntAccount:data.ntAccount});
-          this.setState({department:data.department});
-          this.setState({careCenter:data.careCenter});
-          this.setState({jobTitle:data.jobTitle});
-          this.setState({hiringDate:data.hiringDate});
-          this.setState({mobNumber:data.mobNumber});
-        })
+      .then((data) => {
+        console.log(data);
+        if (data) {
+          this.setState({ staffId: data.staffId });
+          this.setState({ employeeName: data.employeeName });
+          this.setState({ managerName: data.managerName });
+          this.setState({ ntAccount: data.ntAccount });
+          this.setState({ department: data.department });
+          this.setState({ careCenter: data.careCenter });
+          this.setState({ jobTitle: data.jobTitle });
+          this.setState({ hiringDate: data.hiringDate });
+          this.setState({ mobNumber: data.mobNumber });
+        }
+      })
   }
 
   onSubmit = (e) => {
     e.preventDefault();
     fetch(API + SUBMIT, {
       body: JSON.stringify({
-          staffId:this.state.staffId,
+        staffId: this.state.staffId,
+        status: "new",
+        phase1: {
+          status: "done",
+          returnedHeadset: this.state.returnedHeadset,
+          returnedKeys: this.state.returnedKeys,
+          returnedOhda: this.state.returnedOhda,
+          ohdaType: this.state.ohdaType,
+          lastWorkDay: this.state.lastWorkDay,
+          nationalId: this.state.nationalId,
+          nationalIdImg: this.state.nationalIdImg,
+          annualsGranted: this.state.annualsGranted,
+          annualsTaken: this.state.annualsTaken,
+          noShow: this.state.noShow,
+          lostHours: this.state.lostHours,
+          daysToTake: this.state.daysToTake
+        },
+        phase2: {
           status: "new",
-          phase1: {
-            status: "done",
-            returnedHeadset: this.state.returnedHeadset,
-            returnedKeys: this.state.returnedKeys,
-            returnedOhda: this.state.returnedOhda,
-            ohdaType:this.state.ohdaType,
-            lastWorkDay:this.state.lastWorkDay,
-            nationalId:this.state.nationalId,
-            nationalIdImg:this.state.nationalIdImg,
-            annualsGranted:this.state.annualsGranted,
-            annualsTaken:this.state.annualsTaken,
-            noShow:this.state.noShow,
-            lostHours:this.state.lostHours,
-            daysToTake:this.state.daysToTake
-          },
-          phase2: {
-            status: "new",
-          },
-          phase3: {
-            status: "new",
-          },
-          phase4: {
-            status: "new",
-          },
-          phase5: {
-            status: "new",
-          },
-          phase6: {
-            status: "new",
-          },
-          phase7: {
-            status: "new",
-          }
+        },
+        phase3: {
+          status: "new",
+        },
+        phase4: {
+          status: "new",
+        },
+        phase5: {
+          status: "new",
+        },
+        phase6: {
+          status: "new",
+        },
+        phase7: {
+          status: "new",
+        }
       }),
       headers: {
         'content-type': 'application/json'
       },
       method: 'POST',
     })
-      .then( (response) => {
+      .then((response) => {
         console.log('toot-client');
         // console.log(response.json());
       })
   }
 
-  handleChange= e => {
+  handleChange = e => {
     // console.log({[e.target.name]: e.target.value});
-    this.setState({[e.target.name]: e.target.value});
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
     return (
       <Container >
-        <br/>
+        <br />
         <h3>Resignation Request</h3>
-        <br/>
+        <br />
         <Form >
-        <Form.Group >
-              <Form.Group className="p-2 border border-danger">
-                <Row >
+          <Form.Group >
+            <Form.Group className="p-2 border border-danger">
+              <Row >
                 <Col><Form.Label>Staff ID</Form.Label></Col>
                 <Col><Form.Control
-                    name="staffId"
-                    id="id"
-                    placeholder="12345"
-                    className="form-control"
-                    onChange={this.handleChange}
-                  /></Col>
+                  name="staffId"
+                  id="id"
+                  placeholder="12345"
+                  className="form-control"
+                  onChange={this.handleChange}
+                /></Col>
                 <Col>
                   <Button type="button" variant="danger" onClick={this.onSearch}>Search</Button>
                 </Col>
@@ -151,48 +158,48 @@ export class ResignReqScreen extends Component {
             </Form.Group>
             <Row>
               <Col><Form.Label className="col-form-label">SAP Staff ID</Form.Label></Col>
-              <Col ><Form.Control plaintext readOnly value={this.state.sapID}/></Col>
+              <Col ><Form.Control plaintext readOnly value={this.state.sapID} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Employee Name</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.employeeName}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.employeeName} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Manager Name</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.managerName}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.managerName} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>NT Account</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.ntAccount}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.ntAccount} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Department</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.department}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.department} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Care Center</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.careCenter}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.careCenter} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Job Title</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.jobTitle}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.jobTitle} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Hiring Date</Form.Label></Col>
               {/* <Col><Form.Control plaintext readOnly defaultValue="Hiring Date"/></Col> */}
-              <Col><Form.Control plaintext readOnly value={this.state.hiringDate}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.hiringDate} /></Col>
               <Col></Col>
             </Row>
             <Row>
               <Col><Form.Label>Mobile Number</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.mobNumber}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.mobNumber} /></Col>
               <Col></Col>
             </Row>
           </Form.Group>
@@ -200,18 +207,18 @@ export class ResignReqScreen extends Component {
             <Row>
               <Col><Form.Label>Returned Headset</Form.Label></Col>
               <Col>
-              <Form.Control as="select" name="returnedHeadset" onChange={this.handleChange}>
+                <Form.Control as="select" name="returnedHeadset" onChange={this.handleChange}>
                   <option>Yes</option>
                   <option>No</option>
-              </Form.Control>
+                </Form.Control>
               </Col>
             </Row>
             <Row>
               <Col><Form.Label>Returned Keys</Form.Label></Col>
               <Col>
                 <Form.Control as="select" name="returnedKeys" onChange={this.handleChange}>
-                    <option>No</option>
-                    <option>Yes</option>
+                  <option>No</option>
+                  <option>Yes</option>
                 </Form.Control>
               </Col>
             </Row>
@@ -219,14 +226,14 @@ export class ResignReqScreen extends Component {
               <Col><Form.Label>Returned 3ohda</Form.Label></Col>
               <Col>
                 <Form.Control as="select" name="returnedOhda" onChange={this.handleChange} required>
-                    <option>No</option>
-                    <option>Yes</option>
+                  <option>No</option>
+                  <option>Yes</option>
                 </Form.Control>
               </Col>
             </Row>
             <Row>
               <Col><Form.Label>3ohda Type</Form.Label></Col>
-              <Col><Form.Control as="textarea" rows="1" name="ohdaType" onChange={this.handleChange} required/></Col>
+              <Col><Form.Control as="textarea" rows="1" name="ohdaType" onChange={this.handleChange} required /></Col>
             </Row>
           </Form.Group>
           <Form.Group className="p-2 border border-danger">
@@ -234,7 +241,7 @@ export class ResignReqScreen extends Component {
               <Col><Form.Label>Leave Balance</Form.Label></Col>
               <Col></Col>
               <Col><Form.Label>IEX</Form.Label></Col>
-              <Col><Form.Control plaintext readOnly value={this.state.iex}/></Col>
+              <Col><Form.Control plaintext readOnly value={this.state.iex} /></Col>
             </Row>
             <table className="table">
               <thead className="thead-dark">
@@ -248,17 +255,17 @@ export class ResignReqScreen extends Component {
               </thead>
               <tbody>
                 <tr>
-                  <th scope="row"><Form.Control as="textarea" rows="1" name="annualsGranted" onChange={this.handleChange} required/></th>
-                  <td><Form.Control as="textarea" rows="1" name="annualsTaken" onChange={this.handleChange} required/></td>
-                  <td><Form.Control as="textarea" rows="1" name="noShow" onChange={this.handleChange} required/></td>
-                  <td><Form.Control as="textarea" rows="1" name="lostHours" onChange={this.handleChange} required/></td>
-                  <td><Form.Control as="textarea" rows="1" name="daysToTake" onChange={this.handleChange} required/></td>
+                  <th scope="row"><Form.Control as="textarea" rows="1" name="annualsGranted" onChange={this.handleChange} required /></th>
+                  <td><Form.Control as="textarea" rows="1" name="annualsTaken" onChange={this.handleChange} required /></td>
+                  <td><Form.Control as="textarea" rows="1" name="noShow" onChange={this.handleChange} required /></td>
+                  <td><Form.Control as="textarea" rows="1" name="lostHours" onChange={this.handleChange} required /></td>
+                  <td><Form.Control as="textarea" rows="1" name="daysToTake" onChange={this.handleChange} required /></td>
                 </tr>
               </tbody>
             </table>
           </Form.Group>
           <Form.Group className="p-2 border border-danger">
-          <Row>
+            <Row>
               <Col><Form.Label>Last Working Day</Form.Label></Col>
               {/* <Col><Form.Control rows="1" required/></Col> */}
               <Col> <input type="date" id="last" name="lastWorkDay"
@@ -266,16 +273,16 @@ export class ResignReqScreen extends Component {
             </Row>
             <Row>
               <Col><Form.Label>National ID Number</Form.Label></Col>
-              <Col><Form.Control as="textarea" rows="1" name="nationalId" onChange={this.handleChange} required/></Col>
+              <Col><Form.Control as="textarea" rows="1" name="nationalId" onChange={this.handleChange} required /></Col>
             </Row>
           </Form.Group>
           <Form.Group className="p-2 border border-danger">
             <Row>
               <Col><Form.Label>Copy of National ID</Form.Label></Col>
-              <Col><input type="file" className="form-control-file border" name="nationalIdImg" onChange={this.handleChange} required/></Col>
+              <Col><input type="file" className="form-control-file border" name="nationalIdImg" onChange={this.handleChange} required /></Col>
             </Row>
           </Form.Group>
-          <br/>
+          <br />
           <Button type="submit" variant="danger" size="lg" onClick={this.onSubmit} block>Submit</Button>
         </Form>
       </Container>
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,17 +6,6 @@ module.exports = (db) => {
     const collection = "users";
 
     const router = new express.Router();
-    // const temp = {
-    //     "sapID":"12",
-    //     "employeeName":"21",
-    //     "managerName":"212",
-    //     "ntAccount":"54",
-    //     "department":"21",
-    //     "careCenter":"89",
-    //     "jobTitle":"222",
-    //     "hiringDate":"12",
-    //     "mobNumber":"121391",
-    //   };
 
     router.post('/bulkregister', function (req, res) {
         console.log(req.file);
@@ -27,15 +16,15 @@ module.exports = (db) => {
         console.log('toot-server')
         console.log(req.body);
         db.collection(collection)
-          .findOne(req.body)
-          .then(user => {
-            if (!user) {
-                console.log("ID Not found");
-                return;
-            }
-            res.json(user);
-        });
-        // res.json(user);
+            .findOne({ staffId: req.body.staffId })
+            .then(user => {
+                if (!user) {
+                    console.log("ID Not found");
+                    res.status(404).send({});
+                } else {
+                    res.json(user);
+                }
+            });
     });
 
     return router;
