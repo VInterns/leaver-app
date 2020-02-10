3a0627b03ea9ca0f3fbc35e7ef2b5d525cce598b
DaliaZhran <dalia_zhran@hotmail.com>
2020-02-10 17:40:58 +0200

    authorization FE v2

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -8,8 +8,11 @@ export class Header extends React.Component {
   constructor() {
     super();
     this.state = {
-      err: ""
+      err: "",
+      resign_req_users : ["manager","hr"],
+
     };
+    // const resign_req_users = ["manager","hr"];
   }
 
   static mapStateToProps(state) {
@@ -24,6 +27,10 @@ export class Header extends React.Component {
     return bindActionCreators({ logout }, dispatch);
   }
 
+  checkAuth = (allowed_users,user_roles) => {
+    const found = allowed_users.some(r=> user_roles.indexOf(r) >= 0);
+    return found;
+  }
 
   render() {
     if (!this.props.isAuthenticated) {
@@ -34,7 +41,9 @@ export class Header extends React.Component {
         <Navbar.Brand >Leaver App</Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/upload">Upload Users</Nav.Link>
-          <Nav.Link href="/resign">Resignation Request</Nav.Link>
+          {console.log(this.props.account.role)}
+          {this.checkAuth(this.state.resign_req_users,this.props.account.role) && <Nav.Link href="/resign">Resignation Request</Nav.Link>}
+          {/* {this.props.account.role === "finance" && <Nav.Link href="/resign">Resignation Request</Nav.Link>} */}
           <Nav.Link href="/smc">Customer Care</Nav.Link>
           <Nav.Link href="/wf-view">Work Force</Nav.Link>
           <Nav.Link href="/cc-consumer-activation-table">
