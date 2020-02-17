63a0a0fa5d841ba29a00ede77b83369ad0ba79ab
EhabIbrahimH <e.hassan@nu.edu.eg>
2020-02-17 20:48:44 +0200

    Connect to MongoDB Atlas cloud

--- server/utilities.js
+++ server/utilities.js
@@ -1,30 +1,30 @@
 const getDatabaseUrl = () => {
-    return process.env.DATABASE_URL || "mongodb://localhost:27017/leaver-app";
+  return 'mongodb://ehab:mongopassword@vf-shard-00-00-4myzf.mongodb.net:27017,vf-shard-00-01-4myzf.mongodb.net:27017,vf-shard-00-02-4myzf.mongodb.net:27017/leaver-app?ssl=true&replicaSet=VF-shard-0&authSource=admin&retryWrites=true&w=majority';
 };
 
 const getPort = () => {
-    return normalizePort(process.env.PORT || "3000");
-}
+  return normalizePort(process.env.PORT || '3000');
+};
 /**
  * Normalize a port into a number, string, or false.
  */
 
 function normalizePort(val) {
-    const port = parseInt(val, 10);
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
+  const port = parseInt(val, 10);
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
 
-    if (port >= 0) {
-        // port number
-        return port;
-    }
+  if (port >= 0) {
+    // port number
+    return port;
+  }
 
-    return false;
+  return false;
 }
 
 module.exports = {
-    getPort,
-    getDatabaseUrl
+  getPort,
+  getDatabaseUrl
 };
