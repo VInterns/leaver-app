a023a6579adc778afee2047017dc75510bea5833
Ehab Ibrahim <e.hassan@nu.edu.eg>
2020-02-28 00:37:33 +0200

    Fix HR table

--- client/src/screens/hrt-table-screen.js
+++ client/src/screens/hrt-table-screen.js
@@ -53,75 +53,101 @@ export class HrViewScreen extends Component {
     }
   }
 
-    checkRequestStatus (resignation) {
-        if (resignation.phase2.status === "new" && resignation.phase3.status === "new" && resignation.phase4.status === "new" && resignation.phase5.status === "new" && resignation.phase6.status === "new" && resignation.phase7.status === "new" ) {
-            return "new";
-        } else if (resignation.phase2.status === "done" && resignation.phase3.status === "done" && resignation.phase4.status === "done" && resignation.phase5.status === "done" && resignation.phase6.status === "done" && resignation.phase7.status === "done" ) {
-            return "done";
-        } else {
-            return "pending";
-        }
+  checkRequestStatus(resignation) {
+    if (
+      resignation.phase2.status === 'new' &&
+      resignation.phase3.status === 'new' &&
+      resignation.phase4.status === 'new' &&
+      resignation.phase5.status === 'new' &&
+      resignation.phase6.status === 'new' &&
+      resignation.phase7.status === 'new'
+    ) {
+      return 'new';
+    } else if (
+      resignation.phase2.status === 'done' &&
+      resignation.phase3.status === 'done' &&
+      resignation.phase4.status === 'done' &&
+      resignation.phase5.status === 'done' &&
+      resignation.phase6.status === 'done' &&
+      resignation.phase7.status === 'done'
+    ) {
+      return 'done';
+    } else {
+      return 'pending';
     }
+  }
 
-    componentDidMount() {
-        fetch('/api/resignations').then((res) => {
-            res.json().then((data) => {
-                this.setState({ data: data });
-            })
-        })
-    }
+  componentDidMount() {
+    fetch('/api/resignations').then(res => {
+      res.json().then(data => {
+        this.setState({ data: data });
+      });
+    });
+  }
 
-    render() {
-        return (
-            <div className = "container-fluid">
-                <center style = {{margin: "25px"}}>
-                <header className="ast-header"> 
-                    <hr/>
-                    <h3>Human Resources Team</h3>
-                    <hr/>
-                </header>
-                <Table bordered striped hover>
-                <thead>
-                    <tr style = {{backgroundColor: "#BE0002", "whiteSpace": "nowrap"}}>
-                        <th className = "text-white">Staff ID</th>
-                        <th className = "text-white">Employee Name</th>
-                        <th className = "text-white">Last Working Day</th>
-                        <th className = "text-white">Manager Name</th>
-                        <th className = "text-white">Resignation Request</th>
-                        <th className = "text-white">Customer Care</th>
-                        <th className = "text-white">Work Force</th>
-                        <th className = "text-white">CC Consumer Activation</th>
-                        <th className = "text-white">Enterprise Logistics</th>
-                        <th className = "text-white">Application Security</th>
-                        <th className = "text-white">Security Hardware Team</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        this.state.data.map((value, index) => {
-                            return (
-                                <tr onClick={() => {
-                                    this.props.history.push(`/resignations-details?id=${value.staffId}`)
-                                }} key={index}>
-                                    <td>{value.staffId} </td>
-                                    <td>{value.name}</td>
-                                    <td>{value.phase1.lastWorkDay}</td>
-                                    <td >{value.managerName}</td>
-                                    {this.checkStatus(this.checkRequestStatus(value))}
-                                    {this.checkStatus(value.phase2.status)}
-                                    {this.checkStatus(value.phase3.status)}
-                                    {this.checkStatus(value.phase4.status)}
-                                    {this.checkStatus(value.phase5.status)}
-                                    {this.checkStatus(value.phase6.status)}
-                                    {this.checkStatus(value.phase7.status)}
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
-            </Table >
-            </center>
-        </div>
-        )
-    }
+  render() {
+    return (
+      <div className='container-fluid'>
+        <center style={{ margin: '25px' }}>
+          <header className='ast-header'>
+            <hr />
+            <h3>Human Resources Team</h3>
+            <hr />
+          </header>
+
+          <ReactHTMLTableToExcel
+            id='test-table-xls-button'
+            className='download-table-xls-button btn btn-outline-success btn-lg btn-block mt-3 mb-3'
+            table='wf-table'
+            filename='Resignations - Work Force'
+            sheet='resignations'
+            buttonText='Export Table as XLS'
+          />
+          <Table bordered striped hover id='hr-table'>
+            <thead>
+              <tr style={{ backgroundColor: '#BE0002', whiteSpace: 'nowrap' }}>
+                <th className='text-white'>Staff ID</th>
+                <th className='text-white'>Employee Name</th>
+                <th className='text-white'>Last Working Day</th>
+                <th className='text-white'>Manager Name</th>
+                <th className='text-white'>Resignation Request</th>
+                <th className='text-white'>Customer Care</th>
+                <th className='text-white'>Work Force</th>
+                <th className='text-white'>CC Consumer Activation</th>
+                <th className='text-white'>Enterprise Logistics</th>
+                <th className='text-white'>Application Security</th>
+                <th className='text-white'>Security Hardware Team</th>
+              </tr>
+            </thead>
+            <tbody>
+              {this.state.data.map((value, index) => {
+                return (
+                  <tr
+                    onClick={() => {
+                      this.props.history.push(
+                        `/resignations-details?id=${value.staffId}`
+                      );
+                    }}
+                    key={index}
+                  >
+                    <td>{value.staffId} </td>
+                    <td>{value.name}</td>
+                    <td>{value.phase1.lastWorkDay}</td>
+                    <td>{value.managerName}</td>
+                    {this.checkStatus(this.checkRequestStatus(value))}
+                    {this.checkStatus(value.phase2.status)}
+                    {this.checkStatus(value.phase3.status)}
+                    {this.checkStatus(value.phase4.status)}
+                    {this.checkStatus(value.phase5.status)}
+                    {this.checkStatus(value.phase6.status)}
+                    {this.checkStatus(value.phase7.status)}
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </center>
+      </div>
+    );
+  }
 }
