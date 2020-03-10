f5ff75fc03920687ac1ef7028fd59fd40410096e
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2020-03-10 10:06:33 +0200

    edited work force table screen

--- client/src/screens/wft-table-screen.js
+++ client/src/screens/wft-table-screen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Container, Table, Header } from 'semantic-ui-react';
 import ReactHTMLTableToExcel from 'react-html-table-to-excel';
 
 const API = '/api/resignations';
@@ -83,52 +83,52 @@ export class WorkForceScreen extends React.Component {
         throw err;
       });
   }
-
   render() {
     const { requests } = this.state;
     return (
-      <div className='container'>
-        <center style={{ margin: '25px' }}>
-          <header>
+      <Container fluid className="p-5 bg-light" style={{ height: '100vh' }}>
+        <Header as='h3' className='text-center'>Work Force Team</Header>
+        <div className='row'>
+          <div className='offset-md-3 col-md-6 border rounded bg-white'>
+            <div className='row mt-4'>
+              <div className='col d-flex flex-row'>
+                <ReactHTMLTableToExcel
+                  id='test-table-xls-button'
+                  className='download-table-xls-button ui basic button'
+                  table='wf-table'
+                  filename='Resignations - Work Force'
+                  sheet='resignations'
+                  buttonText='Export Table'
+                />
+              </div>
+            </div>
             <hr />
-            <h3>Work Force Team</h3>
-            <hr />
-          </header>
-          <div>
-            <ReactHTMLTableToExcel
-              id='test-table-xls-button'
-              className='download-table-xls-button btn btn-outline-success btn-lg btn-block mt-3 mb-3'
-              table='wf-table'
-              filename='Resignations - Work Force'
-              sheet='resignations'
-              buttonText='Export Table as XLS'
-            />
-            <Table bordered hover striped id='wf-table'>
-              <thead>
-                <tr style={{ backgroundColor: '#BE0002' }}>
-                  <th className='text-white'>Staff ID</th>
-                  <th className='text-white'>Employee Name</th>
-                  <th className='text-white'>Manager Name</th>
-                  <th className='text-white'>Status</th>
-                </tr>
-              </thead>
-              <tbody>
+            <Table celled className='table-hover mb-3' id='wf-table'>
+              <Table.Header>
+                <Table.Row>
+                  <Table.HeaderCell>Staff ID</Table.HeaderCell>
+                  <Table.HeaderCell>Employee Name</Table.HeaderCell>
+                  <Table.HeaderCell>Manager Name</Table.HeaderCell>
+                  <Table.HeaderCell>Status</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
                 {requests.map(request => (
-                  <tr
+                  <Table.Row
                     onClick={() => this.clickButton(request)}
                     key={request.staffId}
                   >
-                    <td>{request.staffId}</td>
-                    <td>{request.name}</td>
-                    <td>{request.managerName}</td>
+                    <Table.Cell>{request.staffId}</Table.Cell>
+                    <Table.Cell>{request.name}</Table.Cell>
+                    <Table.Cell>{request.managerName}</Table.Cell>
                     {this.checkStatus(request.phase3.status)}
-                  </tr>
+                  </Table.Row>
                 ))}
-              </tbody>
+              </Table.Body>
             </Table>
           </div>
-        </center>
-      </div>
+        </div>
+      </Container>
     );
   }
 }
