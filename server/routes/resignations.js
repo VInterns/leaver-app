56240985e6178c7e9c34b80582ae176735303272
Islam Gad <i_gad_@hotmail.com>
2020-05-03 04:35:53 +0200

    Update and refactor resignations route

--- server/routes/resignations.js
+++ server/routes/resignations.js
@@ -11,24 +11,77 @@ module.exports = db => {
   const router = new Router();
   const collection = "resignations";
 
-  // TODO: Also HR should be able to access this endpoint
-  router.get("/", ensureLoggedIn, ensureHasRole(["admin"]), (req, res) => {
-    db.collection(collection)
-      .find()
-      .toArray((err, data) => {
-        if (err) {
-          res.status(500).send();
-        } else {
-          res.json(data);
-        }
-      });
-  });
+  router.get("/",
+    ensureLoggedIn,
+    ensureHasRole(["admin", "hr"]),
+    (req, res) => {
+      db.collection(collection)
+        .find()
+        .toArray((err, data) => {
+          if (err) {
+            res.status(500).send();
+          } else {
+            res.json(data);
+          }
+        });
+    });
 
-  router.delete(
-    "/delete-request/:id",
+  router.post("/",
     ensureLoggedIn,
     ensureHasRole(["admin"]),
-    function (req, res) {
+    (req, res) => {
+      db.collection(collection)
+        .findOne({ staffId: req.body.staffId })
+        .then(resigReg => {
+          if (!resigReg) {
+            db.collection(collection).insertOne(req.body, (err, result) => {
+              if (err) {
+                res.status(503).send({ error: "Insert to db error:(" });
+                throw err;
+              } else {
+                res.status(200).send({ error: "Heeh :)" });
+                db.collection("employees")
+                  .findOne({ staffId: req.body.staffId })
+                  .then(employee => {
+                    employee.mobile = req.body.mobile;
+                    db.collection("employees").updateOne(employee);
+                  });
+                // Mail Notifications
+                // TODO: add other teams roles & employee email to the mailList
+                let subject = `New Resignation Request for Staff ID# ${req.body.staffId}`;
+                let query = { roles: { $in: ["hr", "vendor", "manager"] } };
+                let toMailListPromise = getMailingList(query).then(mailList => {
+                  let scope = {
+                    name: req.body.name,
+                    staffId: req.body.staffId,
+                    managerName: req.body.managerName,
+                    lastDay: req.body.phase1.lastWorkDay
+                  };
+                  let htmlBodyPromise = getHtmlBody(
+                    "initial-vendor-notification.html",
+                    scope
+                  ).then(htmlBody => {
+                    mailer.sendEmail(mailList, subject, htmlBody, () => {
+                      console.log(
+                        `Resignation Request Creation Email Sent to ${toMailList}`
+                      );
+                    });
+                  });
+                });
+              }
+            });
+          } else {
+            res
+              .status(404)
+              .send({ error: "Resignation Request already exists:(" });
+          }
+        });
+    });
+
+  router.delete("/delete-request/:id",
+    ensureLoggedIn,
+    ensureHasRole(["admin", "manager"]),
+    (req, res) => {
       let urlSections = req.url.split("/");
       let staffId_ = urlSections[urlSections.length - 1];
       db.collection(collection).deleteOne(
@@ -51,7 +104,7 @@ module.exports = db => {
           // TODO: add other teams roles & employee email to the mailList
           let request = req.body.request;
           let subject = `Resignation Request Cancelled for Staff ID# ${request.staffId}`;
-          let query = { roles: { $in: ["hr", "vendor", "manager"] } };
+          let query = { roles: { $in: ["hr", "vendor", "manager", "smc", "cc", "ast", "wf", "cs"] } };
           let toMailListPromise = getMailingList(query).then(mailList => {
             let scope = {
               name: request.name,
@@ -63,11 +116,12 @@ module.exports = db => {
               "initial-vendor-notification.html",
               scope
             ).then(htmlBody => {
-              mailer.sendEmail(mailList, subject, htmlBody, () => {
-                console.log(
-                  `Resignation Request Deletion Email Sent to ${toMailList}`
-                );
-              });
+              console.log(mailList);
+              // mailer.sendEmail(mailList, subject, htmlBody, () => {
+              //   console.log(
+              //     `Resignation Request Deletion Email Sent to ${toMailList}`
+              //   );
+              // });
             });
           });
         }
@@ -75,69 +129,17 @@ module.exports = db => {
     }
   );
 
-  router.post("/", ensureLoggedIn, ensureHasRole(["admin"]), function (
-    req,
-    res
-  ) {
-    db.collection(collection)
-      .findOne({ staffId: req.body.staffId })
-      .then(resigReg => {
-        if (!resigReg) {
-          db.collection(collection).insertOne(req.body, (err, result) => {
-            if (err) {
-              res.status(503).send({ error: "Insert to db error:(" });
-              throw err;
-            } else {
-              res.status(200).send({ error: "Heeh :)" });
-              db.collection("employees")
-                .findOne({ staffId: req.body.staffId })
-                .then(employee => {
-                  employee.mobile = req.body.mobile;
-                  db.collection("employees").updateOne(employee);
-                });
-              // Mail Notifications
-              // TODO: add other teams roles & employee email to the mailList
-              let subject = `New Resignation Request for Staff ID# ${req.body.staffId}`;
-              let query = { roles: { $in: ["hr", "vendor", "manager"] } };
-              let toMailListPromise = getMailingList(query).then(mailList => {
-                let scope = {
-                  name: req.body.name,
-                  staffId: req.body.staffId,
-                  managerName: req.body.managerName,
-                  lastDay: req.body.phase1.lastWorkDay
-                };
-                let htmlBodyPromise = getHtmlBody(
-                  "initial-vendor-notification.html",
-                  scope
-                ).then(htmlBody => {
-                  mailer.sendEmail(mailList, subject, htmlBody, () => {
-                    console.log(
-                      `Resignation Request Creation Email Sent to ${toMailList}`
-                    );
-                  });
-                });
-              });
-            }
-          });
-        } else {
-          res
-            .status(404)
-            .send({ error: "Resignation Request already exists:(" });
-        }
-      });
-  });
 
   // Update Phase 1
   // TODO:
-  //    - Only manager and Admin Should access this endpoint
   //    - change the method to be PUT
   //    - url to be "/"
   //    - to be Generic for all updates
   router.post(
     "/update/request",
     ensureLoggedIn,
-    ensureHasRole(["admin"]),
-    function (req, res) {
+    ensureHasRole(["admin", "manager"]),
+    (req, res) => {
       var leaverId = req.body.staffId;
       db.collection(collection).findOneAndUpdate(
         { staffId: leaverId },
@@ -184,11 +186,10 @@ module.exports = db => {
 
   // TODO:
   //    - Mention where this url is being used
-  //    - Admin all other roles should be able to access it and view their requests / assigned requests only
+  //    - Admin all other roles should be able to view their assigned requests only
   router.get(
     "/pending",
     ensureLoggedIn,
-    ensureHasRole(["admin"]),
     (req, res) => {
       db.collection(collection)
         .find()
@@ -206,68 +207,72 @@ module.exports = db => {
   // TODO:
   //    - Mention where this url is being used
   //    - Check who should have access to this endpoint
-  router.get("/:id", ensureLoggedIn, ensureHasRole(["admin"]), (req, res) => {
-    let urlSections = req.url.split("/");
-    urlSections[urlSections.length - 1] + "url";
-    var query = { staffId: Number(urlSections[urlSections.length - 1]) };
-    query;
-    db.collection(collection).findOne(query, (err, data) => {
-      if (err) {
-        err;
-        res.status(500).send();
-      } else {
-        data;
-        res.json(data);
-      }
+  router.get("/:id",
+    ensureLoggedIn,
+    (req, res) => {
+      let urlSections = req.url.split("/");
+      urlSections[urlSections.length - 1] + "url";
+      var query = { staffId: Number(urlSections[urlSections.length - 1]) };
+      query;
+      db.collection(collection).findOne(query, (err, data) => {
+        if (err) {
+          err;
+          res.status(500).send();
+        } else {
+          data;
+          res.json(data);
+        }
+      });
     });
-  });
 
   // Update Phase 4
   // TODO:
   //    - To be deleted and move its functionality to the only update method above.
-  router.post("/data", ensureLoggedIn, ensureHasRole(["admin"]), (req, res) => {
-    let myquery = {
-      staffId: Number(req.query.id)
-    };
-    let phase4 = req.body.phase4;
-    let newvalues = {
-      $set: {
-        phase4: phase4,
-        status: req.body.status
-      }
-    };
-    db.collection(collection)
-      .updateOne(myquery, newvalues)
-      .then(result => {
-        /******* MAIL NOTIFICATION *******/
-        // TODO: add any other required emails
-        if (phase4.status === "done") {
-          let searchQuery = { roles: { $in: ["hr", "manager"] } };
-          let mailListPromise = getMailingList(searchQuery).then(emails => {
-            let scope = {
-              staffId: req.query.id,
-              teamName: "CC  Consumer Activation"
-            };
-            let htmlBodyPromise = getHtmlBody("phase-update.html", scope).then(
-              htmlBody => {
-                let subject = `Phase Update for Staff ID #${req.body.id}`;
-                mailer.sendEmail(emails, subject, htmlBody, () => {
-                  console.log(`Phase update email sent to ${emails}`);
-                });
-              }
-            );
-          });
-        } else {
-          console.log("Phase 4 is not done yet.");
+  router.post("/data",
+    ensureLoggedIn,
+    ensureHasRole(["admin"]), (req, res) => {
+      let myquery = {
+        staffId: Number(req.query.id)
+      };
+      let phase4 = req.body.phase4;
+      let newvalues = {
+        $set: {
+          phase4: phase4,
+          status: req.body.status
         }
+      };
+      db.collection(collection)
+        .updateOne(myquery, newvalues)
+        .then(result => {
+          /******* MAIL NOTIFICATION *******/
+          // TODO: add any other required emails
+          if (phase4.status === "done") {
+            let searchQuery = { roles: { $in: ["hr", "manager"] } };
+            let mailListPromise = getMailingList(searchQuery).then(emails => {
+              let scope = {
+                staffId: req.query.id,
+                teamName: "CC  Consumer Activation"
+              };
+              let htmlBodyPromise = getHtmlBody("phase-update.html", scope).then(
+                htmlBody => {
+                  let subject = `Phase Update for Staff ID #${req.body.id}`;
+                  mailer.sendEmail(emails, subject, htmlBody, () => {
+                    console.log(`Phase update email sent to ${emails}`);
+                  });
+                }
+              );
+            });
+          } else {
+            console.log("Phase 4 is not done yet.");
+          }
 
-        res.status(200).send(true);
-      })
-      .catch(err => {
-        console.error(`Failed to update: ${err}`);
-        res.status(500).send();
-      });
-  });
+          res.status(200).send(true);
+        })
+        .catch(err => {
+          console.error(`Failed to update: ${err}`);
+          res.status(500).send();
+        });
+    });
 
   // TODO: @Islam
   //    - make it more clear and add more description to what is happening here.
@@ -277,7 +282,7 @@ module.exports = db => {
     ensureLoggedIn,
     ensureHasRole(["admin"]),
     (req, res) => {
-      res.status(200).send({ msg: "hi" });
+      res.status(200).send({ validImage: true });
     }
   );
 
@@ -373,77 +378,77 @@ module.exports = db => {
   // Update phase 8
   // TODO:
   //    - To be deleted and use the global generic one.
-  router.post("/update/cs", ensureLoggedIn, ensureHasRole(["admin"]), function (
-    req,
-    res
-  ) {
-    var leaverId = req.body.staffId;
-    db.collection(collection).findOneAndUpdate(
-      { staffId: leaverId },
-      {
-        $set: {
-          phase8: req.body.phase8,
-          status: req.body.status
-        }
-      },
-      function (err, doc) {
-        if (err) {
-          res.status(404).send();
-          throw err;
-        } else {
-          //req.body.phase8.status === 'done' && mailer.sendPhaseUpdate();
+  router.post("/update/cs",
+    ensureLoggedIn,
+    ensureHasRole(["admin"]),
+    (req, res) => {
+      var leaverId = req.body.staffId;
+      db.collection(collection).findOneAndUpdate(
+        { staffId: leaverId },
+        {
+          $set: {
+            phase8: req.body.phase8,
+            status: req.body.status
+          }
+        },
+        function (err, doc) {
+          if (err) {
+            res.status(404).send();
+            throw err;
+          } else {
+            //req.body.phase8.status === 'done' && mailer.sendPhaseUpdate();
 
-          /******* MAIL NOTIFICATION *******/
-          // TODO: add any other required emails
-          if (req.body.phase8.status === "done") {
-            let searchQuery = { roles: { $in: ["hr", "manager"] } };
-            let mailListPromise = getMailingList(searchQuery).then(emails => {
-              let scope = { staffId: leaverId, teamName: "Corporate Security" };
-              let htmlBodyPromise = getHtmlBody(
-                "phase-update.html",
-                scope
-              ).then(htmlBody => {
-                let subject = `Phase Update for Staff ID #${leaverId}`;
-                mailer.sendEmail(emails, subject, htmlBody, () => {
-                  console.log(`Phase update email sent to ${emails}`);
-                });
-              });
-            });
-
-            // check if all phases are done
-            if (req.body.status === "done") {
-              let searchQuery = { roles: { $in: ["hr", "manager", "vendor"] } };
+            /******* MAIL NOTIFICATION *******/
+            // TODO: add any other required emails
+            if (req.body.phase8.status === "done") {
+              let searchQuery = { roles: { $in: ["hr", "manager"] } };
               let mailListPromise = getMailingList(searchQuery).then(emails => {
-                let scope = { staffId: leaverId };
+                let scope = { staffId: leaverId, teamName: "Corporate Security" };
                 let htmlBodyPromise = getHtmlBody(
-                  "resignation-done.html",
+                  "phase-update.html",
                   scope
                 ).then(htmlBody => {
-                  let subject = `Resignation Request for Staff ID #${leaverId} Finished`;
+                  let subject = `Phase Update for Staff ID #${leaverId}`;
                   mailer.sendEmail(emails, subject, htmlBody, () => {
-                    console.log(
-                      `Resignation Request finish email sent to ${emails}`
-                    );
+                    console.log(`Phase update email sent to ${emails}`);
                   });
                 });
               });
-            } else {
-              console.log(
-                `Resignation Request for Staff ID #${leaverId} hasn't finished yet.`
-              );
-            }
-          } else {
-            console.log("Phase 8 is not done yet.");
-          }
 
-          res.status(200).send({
-            msg:
-              "employee successfully found, and corporate security data successfully updated"
-          });
+              // check if all phases are done
+              if (req.body.status === "done") {
+                let searchQuery = { roles: { $in: ["hr", "manager", "vendor"] } };
+                let mailListPromise = getMailingList(searchQuery).then(emails => {
+                  let scope = { staffId: leaverId };
+                  let htmlBodyPromise = getHtmlBody(
+                    "resignation-done.html",
+                    scope
+                  ).then(htmlBody => {
+                    let subject = `Resignation Request for Staff ID #${leaverId} Finished`;
+                    mailer.sendEmail(emails, subject, htmlBody, () => {
+                      console.log(
+                        `Resignation Request finish email sent to ${emails}`
+                      );
+                    });
+                  });
+                });
+              } else {
+                console.log(
+                  `Resignation Request for Staff ID #${leaverId} hasn't finished yet.`
+                );
+              }
+            } else {
+              console.log("Phase 8 is not done yet.");
+            }
+
+            res.status(200).send({
+              msg:
+                "employee successfully found, and corporate security data successfully updated"
+            });
+          }
         }
-      }
-    );
-  });
+      );
+    });
 
   // Update phase 6
   // TODO:
@@ -452,7 +457,7 @@ module.exports = db => {
     "/update/phase6",
     ensureLoggedIn,
     ensureHasRole(["admin"]),
-    function (req, res) {
+    (req, res) => {
       var leaverId = req.body.staffId;
       db.collection(collection).findOneAndUpdate(
         { staffId: leaverId },
@@ -507,7 +512,7 @@ module.exports = db => {
     "/update/phase2",
     ensureLoggedIn,
     ensureHasRole(["admin"]),
-    function (req, res) {
+    (req, res) => {
       var leaverId = req.body.staffId;
       db.collection(collection).findOneAndUpdate(
         { staffId: leaverId },
@@ -556,7 +561,7 @@ module.exports = db => {
     "/update/phase7",
     ensureLoggedIn,
     ensureHasRole(["admin"]),
-    function (req, res) {
+    (req, res) => {
       var leaverId = req.body.staffId;
       db.collection(collection).findOneAndUpdate(
         { staffId: leaverId },
